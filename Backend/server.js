7af9f4197818cb76067fdfff33cf439ecbcd1c1c
MaizaAymen <aymenmaiza87@gmail.com>
2025-03-17 22:05:22 +0100

    Update CORS configuration to allow multiple origins

--- Backend/server.js
+++ Backend/server.js
@@ -7,10 +7,11 @@ require('dotenv').config()
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://study-with-me-eight.vercel.app/', // Remplace par l'URL de ton frontend
+    origin: ['https://study-with-me-eight.vercel.app', 'https://study-with-me-7otu.vercel.app'], 
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
 }));
+
 
 app.listen(port, () => {
     console.log('Server is running on port 4000');
